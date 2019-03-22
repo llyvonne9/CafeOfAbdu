07b2b62e5c35056f5e0955b528a2c7b94daf916f
Vyom Thakkar <vyomthakkar@wirelessprv-10-192-163-112.near.illinois.edu>
2019-03-21 19:14:16 -0500

    First Commit Dish Restaurant models added

---
+++ FoodEase/restaurant/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    location = models.TextField()
+    url = models.TextField()
+    phone = models.CharField(max_length=50)
